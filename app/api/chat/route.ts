e68bf274f01d15fdf13a57a21f60ec8fe609a7c7
Yoonhyoung-Lee <54613623+Yoonhyoung-Lee@users.noreply.github.com>
2023-12-13 18:14:53 +0900

    feat: add document Retrieval QA

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,29 +1,99 @@
-import { StreamingTextResponse, LangChainStream, Message } from 'ai';
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { AIMessage, HumanMessage } from 'langchain/schema';
+import { StreamingTextResponse } from "ai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { Document } from "langchain/document";
+import { introduction } from "@/app/data/docs/introduction";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { StringOutputParser } from "langchain/schema/output_parser";
+import { formatDocumentsAsString } from "langchain/util/document";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate,
+} from "langchain/prompts";
+import { RunnableSequence } from "langchain/schema/runnable";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const { stream, handlers } = LangChainStream();
+  // const { stream, handlers } = LangChainStream();
 
   const llm = new ChatOpenAI({
     streaming: true,
   });
+  console.log(messages);
 
-  llm
-    .call(
-      (messages as Message[]).map(m =>
-        m.role == 'user'
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content),
-      ),
-      {},
-      [handlers],
-    )
-    .catch(console.error);
+  const docs = [introduction];
+  const docOutput = await splitDocs(docs);
+  const vectorStore = await MemoryVectorStore.fromDocuments(
+    docOutput,
+    new OpenAIEmbeddings()
+  );
+  const retriever = vectorStore.asRetriever();
 
-  return new StreamingTextResponse(stream);
+  // Create a system & human prompt for the chat model
+  const SYSTEM_TEMPLATE = `Use the following pieces of context to answer the users question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+----------------
+{context}`;
+
+  const promptTemplates = [
+    SystemMessagePromptTemplate.fromTemplate(SYSTEM_TEMPLATE),
+    HumanMessagePromptTemplate.fromTemplate("{question}"),
+  ];
+  const prompt = ChatPromptTemplate.fromMessages(promptTemplates);
+
+  const chain = RunnableSequence.from([
+    {
+      // Extract the "question" field from the input object and pass it to the retriever as a string
+      sourceDocuments: RunnableSequence.from([
+        (input) => input.question,
+        retriever,
+      ]),
+      question: (input) => input.question,
+    },
+    {
+      // Pass the source documents through unchanged so that we can return them directly in the final result
+      sourceDocuments: (previousStepResult) =>
+        previousStepResult.sourceDocuments,
+      question: (previousStepResult) => previousStepResult.question,
+      context: (previousStepResult) =>
+        formatDocumentsAsString(previousStepResult.sourceDocuments),
+    },
+    {
+      result: prompt.pipe(llm).pipe(new StringOutputParser()),
+      sourceDocuments: (previousStepResult) =>
+        previousStepResult.sourceDocuments,
+    },
+  ]);
+  // TODO: when using chain.stream, it gives bad quality results
+  const res = await chain.invoke({
+    // use the last message
+    question: messages[messages.length - 1].content,
+  });
+  console.log(res);
+
+  return new StreamingTextResponse(res);
+}
+
+// Cheerio cannot execute JavaScript code on the page, so we cannot use
+// async function readDocs() {
+//   const loader = new CheerioWebBaseLoader("https://docs.monad.xyz/");
+
+//   const docs = await loader.load();
+
+//   return docs;
+// }
+
+async function splitDocs(documents: Document<Record<string, any>>[]) {
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+
+  const docOutput = await splitter.splitDocuments(documents);
+  return docOutput;
 }
---
+++ app/data/docs/introduction.ts
@@ -0,0 +1,17 @@
+import { Document } from "langchain/document";
+
+export const introduction: Document<Record<string, any>> = {
+  metadata: { source: "https://docs.monad.xyz" },
+  pageContent: `Monad is a high-performance Ethereum-compatible L1.  Monad materially advances the efficient frontier in the tradeoff between decentralization and scalability.
+Monad introduces optimizations in four major areas, resulting in a blockchain with throughput of 10,000 transactions per second (tps):
+MonadBFT
+Deferred Execution
+Parallel Execution
+MonadDb
+Monad's improvements address existing bottlenecks while preserving seamless compatibility for application developers (full EVM bytecode compatibility) and users (Ethereum RPC API compatibility). As a result, the rich landscape of Ethereum tooling and applied cryptography research can plug seamlessly into Monad while benefiting from improved throughput and scale:
+applications (any dapp built for Ethereum)
+developer tooling (e.g. Hardhat, Apeworx, Foundry)
+wallets (e.g. MetaMask)
+analytics/indexing (e.g. Etherscan, Dune)
+The Monad client is built with a focus on performance and is written from scratch in C++ and Rust.  The subsequent pages survey the major changes in Monad as well as the interface for users.`,
+};
